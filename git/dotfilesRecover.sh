33935230fd73702f11b3b230405a477c97463a05
Qik Start <qikstart@gmail.com>
2021-05-27 01:43:34 +0300

    Dotfiles and dotfilesrecover.sh

---
+++ git/dotfilesRecover.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+LOG_FILE=~/Desktop/upddateDotfiles.log
+DOTFILES=$HOME/src/github.com/jshelton/dotfiles
+
+declare -a origList=(
+    gitignore_global
+    hyper.js
+    hyper_plugins
+    inputrc
+    tmux.conf
+    doingrc
+    zshrc
+    )
+
+
+declare -a List=(
+    git
+    hyper
+    inputrc
+    tmux
+    doing
+    zsh
+    iTerm2
+    vim
+)
+
+# Confirm with User
+echo "This script will clobber the destination files."
+read -p "Are you sure you wish to proceed? [y/N]" -n 1 -r
+echo    # (optional) move to a new line
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1 # handle exits from shell or function but don't exit interactive shell
+fi
+
+# Proceed through every directory
+
+for directory in "${List[@]}"
+do
+    echo $directory >> "$LOG_FILE"
+    echo "$directory: "
+    dotfileDir="$DOTFILES/$directory"
+
+    if [[ ! -d  "$dotfileDir" ]] ; then
+	echo "The directory $dotfileDir does not exist. Skipping." 
+	continue
+    fi
+    for sourcePath in $(find "$dotfileDir" -iname 'dot-*' -depth 1 |grep -v '~')
+    do
+	sourceFile="${sourcePath##*/}"
+	destFile=".${sourceFile#dot-}"
+	echo "	${destFile}"
+
+	if [[ -d "$HOME/$destFile" ]] ; then
+	    rm -rf "$HOME/$destFile";
+	elif [[ -f "$HOME/$destFile" ]];
+	then
+	    rm "$HOME/$destFile";
+	fi
+	ln -s "$sourcePath" "$HOME/$destFile" ;
+    done
+done
